1b4123a2fad8d61a3efdf6fa68ba108a0ad7fe40
BryantLogan <bryant.logan1@gmail.com>
2023-01-04 08:24:16 -0800

    add max and min value for external port variable

--- mtc-terraform/terraform-docker/variables.tf
+++ mtc-terraform/terraform-docker/variables.tf
@@ -1,20 +1,20 @@
 variable "ext_port" {
   type = list
 
-#   validation {
-#     condition     = var.ext_port <= 65535 && var.ext_port > 0
-#     error_message = "The external port must be in valid port range 0 - 65535."
-#   }
+  validation {
+    condition     = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
+    error_message = "The external port must be in valid port range 0 - 65535."
+  }
 }
 
 variable "int_port" {
   type    = number
   default = 1880
 
-#   validation {
-#     condition     = var.int_port == 1880
-#     error_message = "The internal port must be 1880."
-#   }
+  validation {
+    condition     = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
 }
 
 locals {
