d9f0e440a5ec9ad27d2766f4fadfbf894a2dc127
BryantLogan <bryant.logan1@gmail.com>
2023-01-05 14:18:55 -0800

    module outputs

--- mtc-terraform/terraform-docker/main.tf
+++ mtc-terraform/terraform-docker/main.tf
@@ -11,10 +11,10 @@ resource "random_string" "random" {
 }
 
 module "container" {
-  source = "./container"
-  count = local.container_count
-  name_in  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in = module.image.image_out
+  source      = "./container"
+  count       = local.container_count
+  name_in     = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  image_in    = module.image.image_out
   int_port_in = var.int_port
   ext_port_in = var.ext_port[terraform.workspace][count.index]
 }
