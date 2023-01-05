724b485e8ae18ed5b51b911f4e1bdb9fb4af116f
BryantLogan <bryant.logan1@gmail.com>
2023-01-04 17:16:30 -0800

    add variables and outputs

--- mtc-terraform/docker-quiz/main.tf
+++ mtc-terraform/docker-quiz/main.tf
@@ -14,11 +14,16 @@ resource "docker_image" "container_image" {
 }
 
 resource "docker_container" "container" {
-    name = "grafana-container"
+    count = 2
+    name = "grafana_container-${count.index}"
     image = docker_image.container_image.latest
 
     ports {
         internal = 3000
-        external = 3000
+        external = var.ext_port[count.index]
     }
+}
+
+output "ip-address" {
+    value = [for x in docker_container.container : "${x.name}-${x.ip_address}:${x.ports[0].external}"]
 }
