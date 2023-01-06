c3054f19768049c421af3262d13b4de30ec2bef0
BryantLogan <bryant.logan1@gmail.com>
2023-01-05 21:59:31 -0800

    deploy containers with for_each

--- mtc-terraform/terraform-docker/main.tf
+++ mtc-terraform/terraform-docker/main.tf
@@ -1,10 +1,16 @@
 locals {
   deployment = {
     nodered = {
-      image = var.image["nodered"][terraform.workspace]
+      image          = var.image["nodered"][terraform.workspace]
+      int            = 1880
+      ext            = var.ext_port["nodered"][terraform.workspace]
+      container_path = "/data"
     }
     influxdb = {
-      image = var.image["influxdb"][terraform.workspace]
+      image          = var.image["influxdb"][terraform.workspace]
+      int            = 8086
+      ext            = var.ext_port["influxdb"][terraform.workspace]
+      container_path = "/var/lib/influxdb"
     }
   }
 }
@@ -17,18 +23,18 @@ module "image" {
 
 
 resource "random_string" "random" {
-  count   = local.container_count
-  length  = 4
-  special = false
-  upper   = false
+  for_each = local.deployment
+  length   = 4
+  special  = false
+  upper    = false
 }
 
 module "container" {
   source            = "./container"
-  count             = local.container_count
-  name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in          = module.image["nodered"].image_out
-  int_port_in       = var.int_port
-  ext_port_in       = var.ext_port[terraform.workspace][count.index]
-  container_path_in = "/data"
+  for_each          = local.deployment
+  name_in           = join("-", [each.key, terraform.workspace, random_string.random[each.key].result])
+  image_in          = module.image[each.key].image_out
+  int_port_in       = each.value.int
+  ext_port_in       = each.value.ext[0]
+  container_path_in = each.value.container_path
 }
--- mtc-terraform/terraform-docker/variables.tf
+++ mtc-terraform/terraform-docker/variables.tf
@@ -22,14 +22,14 @@ variable "image" {
 variable "ext_port" {
   type = map(any)
 
-  validation {
-    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
-    error_message = "The external port must be in valid port range 0 - 65535."
-  }
-  validation {
-    condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
-    error_message = "The external port must be in valid port range 1880 - 1979."
-  }
+  # validation {
+  #   condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+  #   error_message = "The external port must be in valid port range 0 - 65535."
+  # }
+  # validation {
+  #   condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+  #   error_message = "The external port must be in valid port range 1880 - 1979."
+  # }
 }
 
 variable "int_port" {
@@ -42,6 +42,6 @@ variable "int_port" {
   }
 }
 
-locals {
-  container_count = length(var.ext_port[terraform.workspace])
-}
+# locals {
+#   container_count = length(var.ext_port[terraform.workspace])
+# }
