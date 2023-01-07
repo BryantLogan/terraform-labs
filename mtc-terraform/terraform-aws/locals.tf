0fc6159144f7d0f06d66328f3523a467d4df6719
BryantLogan <bryant.logan1@gmail.com>
2023-01-07 10:43:10 -0800

    VPC security groups

--- mtc-terraform/terraform-aws/locals.tf
+++ mtc-terraform/terraform-aws/locals.tf
@@ -14,7 +14,26 @@ locals {
           protocol    = "tcp"
           cidr_blocks = [var.access_ip]
         }
+        http = {
+          from        = 80
+          to          = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+    rds = {
+      name        = "rds_sg"
+      description = "Security group for RDS"
+      ingress = {
+        mysql = {
+          from        = 3306
+          to          = 3306
+          protocol    = "tcp"
+          cidr_blocks = [local.vpc_cidr]
+        }
       }
     }
   }
 }
+
