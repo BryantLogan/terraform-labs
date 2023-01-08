770f880a3098c9689978e414a4004b541028cbee
BryantLogan <bryant.logan1@gmail.com>
2023-01-08 10:37:05 -0800

    ignore_changes and create_before_destroy

--- mtc-terraform/terraform-aws/loadbalancing/main.tf
+++ mtc-terraform/terraform-aws/loadbalancing/main.tf
@@ -12,6 +12,10 @@ resource "aws_lb_target_group" "mtc_tg" {
   port = var.tg_port
   protocol = var.tg_protocol # "HTTP"
   vpc_id = var.vpc_id
+  lifecycle {
+    ignore_changes = [name]
+    create_before_destroy = true
+  }
   health_check {
     healthy_threshold = var.lb_healthy_threshold #2
     unhealthy_threshold = var.lb_unhealthy_threshold #2
