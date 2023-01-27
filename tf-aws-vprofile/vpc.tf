18eb6c2e4584ef17027d9ec1fc757c472bee3eba
BryantLogan <bryant.logan1@gmail.com>
2023-01-27 11:03:05 -0500

    vpc module

--- tf-aws-vprofile/vpc.tf
+++ tf-aws-vprofile/vpc.tf
@@ -1,23 +1,23 @@
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-    name = var.vpc_name
-    cidr = var.vpc_cidr
-    azs = [var.zone1, var.zone2, var.zone3]
-    public_subnets = [var.pubsubcidr1, var.pubsubcidr2, var.pubsubcidr3]
-    private_subnets = [var.privsubcidr1, var.privsubcidr2, var.privsubcidr3]
+  name            = var.vpc_name
+  cidr            = var.vpc_cidr
+  azs             = [var.zone1, var.zone2, var.zone3]
+  public_subnets  = [var.pubsubcidr1, var.pubsubcidr2, var.pubsubcidr3]
+  private_subnets = [var.privsubcidr1, var.privsubcidr2, var.privsubcidr3]
 
-    enable_nat_gateway = true
-    single_nat_gateway = true
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Terraform = "true"
-        Environment = "Prod"
-    }
+  tags = {
+    Terraform   = "true"
+    Environment = "Prod"
+  }
 
-    vpc_tags = {
-        Name = var.vpc_name
-    }
+  vpc_tags = {
+    Name = var.vpc_name
+  }
 }
